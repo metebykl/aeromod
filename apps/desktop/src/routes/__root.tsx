0d99421641a151efdb067ad1640b8ad0d3d01fbe
Mete <69116434+metebykl@users.noreply.github.com>
2025-04-25 16:04:31 +0300

    fix: disable context menu in production

--- apps/desktop/src/routes/__root.tsx
+++ apps/desktop/src/routes/__root.tsx
@@ -1,10 +1,18 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { useEventListener } from "@/hooks/use-event-listener";
 
 export const Route = createRootRoute({
   component: Root,
 });
 
 function Root() {
+  useEventListener("contextmenu", (e) => {
+    // Disable context menu for production
+    if (import.meta.env.PROD) {
+      e.preventDefault();
+    }
+  });
+
   return (
     <>
       <Outlet />
